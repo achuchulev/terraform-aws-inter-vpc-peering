3e95ce27013a57bd3d81e2268cdc1b2c5696d069
Atanas Chuchulev <atanas.v4@gmail.com>
2020-12-08 18:13:16 +0200

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -7,14 +7,6 @@ output "requester_vpc_id" {
 output "accepter_vpc_id" {
   value = module.vpc_accepter.vpc_id
 }
-
-output "requester_vpc_name" {
-  value = module.vpc_requester.vpc_name
-}
-
-output "accepter_vpc_name" {
-  value = module.vpc_accepter.vpc_name
-}
   
 output "requester_subnet_ids" {
   value = module.vpc_requester.subnet_ids
@@ -33,4 +25,3 @@ output "peering_connection_id" {
 output "peering_status" {
   value = module.inter_vpc_peering.accept_status
 }
-
